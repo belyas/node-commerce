6600af8080050844cd1af0ecfdcdf40950233f60
yassine belkaid <yassine.belkaid87@gmail.com>
2019-05-09 18:31:57 +0000

    added product router to main router file

--- server/routes/web/index.js
+++ server/routes/web/index.js
@@ -2,11 +2,13 @@ import HomeRouter from './home';
 import CategoryRouter from './category';
 import AuthRouter from './auth';
 import ProfileRouter from './profile';
+import ProductRouter from './product';
 
 export default HomeRouter;
 
 export {
     CategoryRouter,
     AuthRouter,
-    ProfileRouter
+    ProfileRouter,
+    ProductRouter
 };
