f4ca9f0bb821fc9d9ca378a8178a985e519a57bb
yassine belkaid <yassine.belkaid87@gmail.com>
2019-05-09 17:10:14 +0000

    fixed null issue in public app.js

--- server/public/js/app.js
+++ server/public/js/app.js
@@ -1,5 +1,7 @@
 (function () {
-    document.querySelector('.removeElement').addEventListener('click', function (event) {
+    const removeEl = document.querySelector('.removeElement');
+
+    removeEl && removeEl.addEventListener('click', function (event) {
         const _id = this.getAttribute('data-form-id');
         const getForm = document.getElementById('remove-form-' + _id);
 
