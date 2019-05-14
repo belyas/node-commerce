b94458122196e02ff3812bbe1c43b87e337a1a0b
yassine belkaid <yassine.belkaid87@gmail.com>
2019-05-14 18:43:27 +0000

    removed csrf token header

--- server/middlewares/locals.js
+++ server/middlewares/locals.js
@@ -10,8 +10,6 @@ export default (req, res, next) => {
     res.locals.isAuthenticated = req.session.isLoggedIn;
     const _token = req.csrfToken();
     res.locals.csrfToken = _token;
-    res.cookie('CSRF-TOKEN', _token);
-
     let errMessage = req.flash('error');
     res.locals.errorMessage = errMessage.length ? errMessage[0] : null;
     let successMessage = req.flash('success');
