3762fc36b8522d0659c397ac9a969a9383fc7e05
yassine belkaid <yassine.belkaid87@gmail.com>
2019-05-18 00:32:25 +0000

    added bootstrap/reactstrap and created the navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,28 @@
 import React from 'react';
-import './App.css';
+import { Navbar, NavbarBrand, NavItem, NavLink, Nav }  from 'reactstrap';
 
 function App() {
   return (
     <div>
-      <header>
-        <p style={{
+      <Navbar color="dark" dark expand="md">
+       <NavbarBrand href="/">Node commerce</NavbarBrand>
+        <Nav className="ml-auto" navbar>
+          <NavItem>
+            <NavLink href="/login">Login</NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink href="/singup">Signup</NavLink>
+          </NavItem>
+        </Nav>
+      </Navbar>
+
+      <p style={{
           display: 'flex',
           alignContent: 'center',
           justifyContent: 'center'
         }}>
           Node commerce front end
         </p>
-      </header>
     </div>
   );
 }
