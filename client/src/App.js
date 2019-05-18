447d85ad33e8dfcb080ee2c6340948d22d97fc58
yassine belkaid <yassine.belkaid87@gmail.com>
2019-05-18 00:08:14 +0000

    installed client app

---
+++ client/src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <header>
+        <p style={{
+          display: 'flex',
+          alignContent: 'center',
+          justifyContent: 'center'
+        }}>
+          Node commerce front end
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
