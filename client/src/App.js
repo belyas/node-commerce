61dac2816147e3345543388a8048e687620180e7
yassine belkaid <yassine.belkaid87@gmail.com>
2019-05-19 18:44:21 +0000

    [client] added routes to App file

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,33 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
 import Header from './components/Header/Header';
 import Menu from './components/Menu/Menu';
+import Login from './components/Auth/Login/Login';
+import Signup from './components/Auth/Signup/Signup';
 
 function App() {
-  return (
-    <div>
-      <Header />
-      <Menu />
-      <p style={{
-          display: 'flex',
-          alignContent: 'center',
-          justifyContent: 'center'
-        }}>
-          Node commerce front end
-        </p>
-    </div>
-  );
+    return (
+        <Router>
+            <div>
+                <Header />
+                <Menu />
+                <p
+                    style={{
+                        display: 'flex',
+                        alignContent: 'center',
+                        justifyContent: 'center',
+                    }}>
+                    Node commerce front end
+                </p>
+                <Switch>
+                    <Route path="/" exact component={() => <div>Home</div>} />
+                    <Route path="/login" component={Login} />
+                    <Route path="/signup" component={Signup} />
+                </Switch>
+            </div>
+        </Router>
+    );
 }
 
 export default App;
