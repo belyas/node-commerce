e4579d8ed0a5477b070453dc44d96812c8749b18
yassine belkaid <yassine.belkaid87@gmail.com>
2019-05-19 18:42:36 +0000

    [client] added Menu component

--- client/src/components/Menu/Menu.js
+++ client/src/components/Menu/Menu.js
@@ -1,39 +1,40 @@
-import React, { useEffect, useState } from "react";
-import { Nav, Navbar, NavItem, NavLink } from "reactstrap";
-import axios from "../../axios";
+import React, { useEffect, useState } from 'react';
+import { NavLink } from 'react-router-dom';
+
+import axios from '../../axios';
 
 const Menu = props => {
-  const [categories, setCategories] = useState([]);
+    const [categories, setCategories] = useState([]);
 
-  useEffect(() => {
-    axios
-      .get("/categories")
-      .then(res => res.data)
-      .then(res => {
-        setCategories(res.data);
-      })
-      .catch(console.log);
-  }, []);
+    useEffect(() => {
+        axios
+            .get('/categories')
+            .then(res => res.data)
+            .then(res => {
+                setCategories(res.data);
+            })
+            .catch(console.log);
+    }, []);
 
-  const categoriesMenu =
-    categories.length &&
-    categories.map(category => {
-      return (
-        <NavItem key={category._id}>
-          <NavLink href={`/category/${category._id}`}>
-            {category.name}
-          </NavLink>
-        </NavItem>
-      );
-    });
-
-  return (
-    <Navbar color="light" light expand="md">
-      <Nav className="m-auto" navbar>
-        {categoriesMenu}
-      </Nav>
-    </Navbar>
-  );
+    return (
+        <div className="navbar navbar-expand-md navbar-light bg-light justify-content-center">
+            <ul className="nav">
+                {categories.length &&
+                    categories.map(category => {
+                        return (
+                            <li className="nav-item" key={category._id}>
+                                <NavLink
+                                    className="nav-link"
+                                    to={`/category/${category._id}`}>
+                                    {' '}
+                                    {category.name}
+                                </NavLink>
+                            </li>
+                        );
+                    })}
+            </ul>
+        </div>
+    );
 };
 
 export default Menu;
