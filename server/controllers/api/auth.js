f40accaec60b9f1fc2128cf24f3a9269386c3ead
yassine belkaid <yassine.belkaid87@gmail.com>
2019-05-23 19:19:09 +0000

    [server] formatting fixes

--- server/controllers/api/auth.js
+++ server/controllers/api/auth.js
@@ -3,35 +3,38 @@ import jwt from 'jsonwebtoken';
 import UserModel from '../../models/user';
 
 export default class Auth {
-    static async login (req, res) {
+    static async login(req, res) {
         const { email, password } = req.body;
 
         try {
             const user = await UserModel.findOne({ email });
-            
+
             if (!user) {
-                return res.status(401).json({ error: 'Something went wrong.', user });
+                return res
+                    .status(401)
+                    .json({ error: 'Something went wrong.', user });
             }
-        
+
             const isEqual = await bcrypt.compare(password, user.password);
-        
+
             if (!isEqual) {
                 return res.status(401).json({ error: 'Something went wrong.' });
             }
-        
+
             const token = jwt.sign(
                 {
                     firstname: user.firstname,
                     lastname: user.lastname,
                     email: user.email,
-                    userId: user._id.toString()
-                }, process.env.JWT_SECRET,
+                    userId: user._id.toString(),
+                },
+                process.env.JWT_SECRET,
                 { expiresIn: '1d' }
             );
-        
+
             res.status(200).json({ token, userId: user._id.toString() });
         } catch (err) {
-            return res.status(500).json({ error: err.message })
+            return res.status(500).json({ error: err.message });
         }
     }
 }
