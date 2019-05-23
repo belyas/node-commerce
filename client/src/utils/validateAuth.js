b8a78067c134ffaccd9572b5dcb499e84a14cf72
yassine belkaid <yassine.belkaid87@gmail.com>
2019-05-23 19:15:38 +0000

    [client] refactored validate atuh

--- client/src/utils/validateAuth.js
+++ client/src/utils/validateAuth.js
@@ -1,18 +1,21 @@
+const PASSWORD_MIN_CHARS = 6;
+const isEmail = email => /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(email);
+
 const validateLoginAuth = values => {
     const errors = {};
 
     // Email Errors
     if (!values.email) {
         errors.email = 'Required Email';
-    } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)) {
+    } else if (!isEmail(values.email)) {
         errors.email = 'Invalid email address';
     }
 
     // Password Errors
     if (!values.password) {
         errors.password = 'Required Password';
-    } else if (values.password.length < 6) {
-        errors.password = 'Password must be at least 6 characters';
+    } else if (values.password.length < PASSWORD_MIN_CHARS) {
+        errors.password = `Password must be at least ${PASSWORD_MIN_CHARS} characters`;
     }
 
     return errors;
