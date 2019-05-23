37d24f4011155549e9d054aa115119f764baacc5
yassine belkaid <yassine.belkaid87@gmail.com>
2019-05-23 01:44:38 +0000

    [client] implemented form validation on login form

--- client/src/components/Auth/Login/Login.js
+++ client/src/components/Auth/Login/Login.js
@@ -1,7 +1,14 @@
 import React, { useState } from 'react';
 
 import { concatClasses } from '../../../utils/helpers';
+import useFormValidation from '../../../hooks/useFormValidation';
+import { validateLoginAuth } from '../../../utils/validateAuth';
 import styles from './Login.module.css';
+
+const INITIAL_STATE = {
+    email: '',
+    password: '',
+};
 
 const Login = () => {
     let [isLogin, setLogin] = useState(true);
@@ -9,11 +16,21 @@ const Login = () => {
     const toggleLoginHandler = () => {
         setLogin(!isLogin);
     };
+    const {
+        values,
+        onChangeHandler,
+        onSubmitHanlder,
+        onBlurHandler,
+        errors,
+        isSubmitting,
+    } = useFormValidation(INITIAL_STATE, validateLoginAuth);
 
     return (
         <div className={concatClasses(styles.wrapper, styles.fadeInDown)}>
             <div className={concatClasses(styles.autoMg, styles.formContent)}>
-                <form action="#" method="POST" style={isLogin ? {} : hideForm}>
+                <form
+                    onSubmit={onSubmitHanlder}
+                    style={isLogin ? {} : hideForm}>
                     <h2
                         className={concatClasses(
                             styles.fadeIn,
@@ -27,19 +44,32 @@ const Login = () => {
                         className={concatClasses(styles.fadeIn, styles.second)}
                         name="email"
                         placeholder="Email address"
+                        value={values.email}
+                        onChange={onChangeHandler}
+                        onBlur={onBlurHandler}
                         required
                     />
+                    {errors.email && (
+                        <p className="alert alert-danger">{errors.email}</p>
+                    )}
                     <input
                         type="password"
                         className={concatClasses(styles.fadeIn, styles.third)}
                         name="password"
                         placeholder="Password"
+                        value={values.password}
+                        onChange={onChangeHandler}
+                        onBlur={onBlurHandler}
                         required
                     />
+                    {errors.password && (
+                        <p className="alert alert-danger">{errors.password}</p>
+                    )}
                     <input
-                        type="button"
+                        type="submit"
                         className={concatClasses(styles.fadeIn, styles.fourth)}
                         value="Log In"
+                        disabled={isSubmitting}
                     />
                 </form>
 
