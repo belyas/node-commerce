3ff6a79daaae9da576efa0011ed10d975cecbcae
yassine belkaid <yassine.belkaid87@gmail.com>
2019-06-10 17:16:40 +0100

    [client] converted authLogin.js to async/await style

--- client/src/apis/authLogin.js
+++ client/src/apis/authLogin.js
@@ -1,27 +1,26 @@
 import axios from '../axios';
 
-const loginAuth = values => {
-    axios
-        .post('/auth', {
+const loginAuth = async (values) => {
+    try {
+        const res = await axios.post('/auth', {
             email: values.email,
             password: values.password,
-        })
-        .then(res => res.data)
-        .then(res => {
-            if (res.token) {
-                localStorage.setItem('token', res.token);
-                localStorage.setItem('userId', res.userId);
-
-                return true;
-            }
-
-            console.log('Error: ', res.error);
-            return false;
-        })
-        .catch(err => {
-            console.log(`Catch error: ${err}`);
-            return false;
         });
+        const data = await res.data;
+
+        if (data.token) {
+            localStorage.setItem('token', data.token);
+            localStorage.setItem('userId', data.userId);
+    
+            return true;
+        }
+    
+        console.log('Error: ', data.error);
+        return false;
+    } catch (err) {
+        console.log(`Catch error: ${err}`);
+        return false;
+    }
 };
 
 export default loginAuth;
