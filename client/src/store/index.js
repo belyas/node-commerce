776a0c81586308fb02d41c80c89c2744f2391dcd
yassine belkaid <yassine.belkaid87@gmail.com>
2019-06-10 20:22:00 +0100

    [client] added redux store

---
+++ client/src/store/index.js
@@ -0,0 +1,16 @@
+import { createStore, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
+
+import rootReducer from './reducers';
+
+const composeEnhancers =
+    process.env.NODE_ENV === 'development'
+        ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+        : null || compose;
+
+const store = createStore(
+    rootReducer,
+    composeEnhancers(applyMiddleware(thunk))
+);
+
+export default store;
