8aa224b4c1bf1437fb9dfdb733064771a465fbbb
yassine belkaid <yassine.belkaid87@gmail.com>
2019-06-10 20:22:54 +0100

    [client] added action types and action creators

---
+++ client/src/store/actions/auth.js
@@ -0,0 +1,72 @@
+import axios from '../../axios';
+import * as actionTypes from '../actions/actionTypes';
+
+export const authStart = () => {
+    return {
+        type: actionTypes.AUTH_START,
+    };
+};
+
+export const authSuccess = (token, userId) => {
+    return {
+        type: actionTypes.AUTH_SUCCESS,
+        token,
+        userId,
+    };
+};
+
+export const authFailure = error => {
+    return {
+        type: actionTypes.AUTH_FAIL,
+        error,
+    };
+};
+
+export const authLoggedout = () => {
+    localStorage.removeItem('token');
+    localStorage.removeItem('userId');
+
+    return {
+        type: actionTypes.AUTH_LOGOUT,
+    };
+};
+
+export const auth = (email, password) => {
+    return async dispatch => {
+        dispatch(authStart());
+
+        try {
+            const res = await axios.post('/auth', {
+                email,
+                password,
+            });
+            const data = await res.data;
+
+            if (data.token) {
+                localStorage.setItem('token', data.token);
+                localStorage.setItem('userId', data.userId);
+                // const expirationDate = new Date(new Date().getTime() + data.expiresIn * 1000);
+                dispatch(authSuccess(data.token, data.userId));
+            } else {
+                dispatch(authFailure(data.error));
+            }
+        } catch (err) {
+            console.log(`Catch error: ${err.message}`);
+            dispatch(authFailure('Invalid email or password'));
+        }
+    };
+};
+
+export const authCheckState = () => {
+    return dispatch => {
+        const token = localStorage.getItem('token');
+
+        if (!token) {
+            dispatch(authLoggedout());
+        } else {
+            const userId = localStorage.getItem('userId');
+
+            dispatch(authSuccess(token, userId));
+        }
+    };
+};
---
+++ client/src/store/actions/index.js
@@ -0,0 +1,8 @@
+export {
+    authStart,
+    authSuccess,
+    authCheckState,
+    authFailure,
+    authLoggedout,
+    auth,
+} from './auth';
