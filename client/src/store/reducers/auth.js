1cf90152da077fddabeded99ad738387a0b34b59
yassine belkaid <yassine.belkaid87@gmail.com>
2019-06-10 20:22:17 +0100

    [client] added store auth reducer

---
+++ client/src/store/reducers/auth.js
@@ -0,0 +1,62 @@
+import * as actionTypes from '../actions/actionTypes';
+
+const initialState = {
+    token: null,
+    userId: null,
+    error: null,
+    loading: false,
+};
+
+const updateObject = (oldObject, newObjectProps) => {
+    return {
+        ...oldObject,
+        ...newObjectProps,
+    };
+};
+
+const authStart = (state, action) => {
+    return updateObject(state, {
+        error: null,
+        loading: true,
+    });
+};
+
+const authSuccess = (state, action) => {
+    return updateObject(state, {
+        error: null,
+        loading: false,
+        token: action.token,
+        userId: action.userId,
+    });
+};
+
+const authFail = (state, action) => {
+    return updateObject(state, {
+        loading: false,
+        error: action.error,
+    });
+};
+
+const authLoggedout = (state, action) => {
+    return updateObject(state, {
+        token: null,
+        userId: null,
+    });
+};
+
+const reducer = (state = initialState, action) => {
+    switch (action.type) {
+        case actionTypes.AUTH_START:
+            return authStart(state, action);
+        case actionTypes.AUTH_SUCCESS:
+            return authSuccess(state, action);
+        case actionTypes.AUTH_FAIL:
+            return authFail(state, action);
+        case actionTypes.AUTH_LOGOUT:
+            return authLoggedout(state, action);
+        default:
+            return state;
+    }
+};
+
+export default reducer;
---
+++ client/src/store/reducers/index.js
@@ -0,0 +1,9 @@
+import { combineReducers } from 'redux';
+
+import AuthReducer from './auth';
+
+const rootReducer = combineReducers({
+    auth: AuthReducer,
+});
+
+export default rootReducer;
