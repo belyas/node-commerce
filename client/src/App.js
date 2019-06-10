ef20e5aa9c1a72236b299b3376663ed81083ff15
yassine belkaid <yassine.belkaid87@gmail.com>
2019-06-10 20:23:52 +0100

    [client] added store and redux devtools to App

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,52 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import React, { Component } from 'react';
+import { Route, Switch, withRouter } from 'react-router-dom';
+import { connect } from 'react-redux';
 
 import Header from './components/Header/Header';
 import Menu from './components/Menu/Menu';
 import Login from './components/Auth/Login/Login';
+import * as actions from './store/actions';
 
-function App() {
-    return (
-        <Router>
+class App extends Component {
+    componentDidMount() {
+        this.props.checkUserStatus();
+    }
+
+    render() {
+        let extraRoutes = null;
+
+        // if (!this.props.isAuthenticated) {
+        extraRoutes = <Route path="/login" component={Login} />;
+        // }
+
+        return (
             <div>
                 <Header />
                 <Menu />
                 <Switch>
                     <Route path="/" exact component={() => <div>Home</div>} />
-                    <Route path="/login" component={Login} />
+                    {extraRoutes}
                 </Switch>
             </div>
-        </Router>
-    );
+        );
+    }
 }
 
-export default App;
+const mapStateToProps = state => {
+    return {
+        isAuthenticated: state.auth.token !== null,
+    };
+};
+
+const mapDispatchToProps = dispatch => {
+    return {
+        checkUserStatus: () => dispatch(actions.authCheckState()),
+    };
+};
+
+export default withRouter(
+    connect(
+        mapStateToProps,
+        mapDispatchToProps
+    )(App)
+);
