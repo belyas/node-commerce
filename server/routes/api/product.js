304e93947298dadb52e2dd109a284f31b7303fbc
yassine belkaid <yassine.belkaid87@gmail.com>
2019-06-18 15:28:24 +0100

    [server] added products by category route

--- server/routes/api/product.js
+++ server/routes/api/product.js
@@ -4,5 +4,6 @@ import { productController } from '../../controllers/api';
 const router = express.Router();
 
 router.get('/', productController.all);
+router.get('/:category_id', productController.getProductsByCategoryId);
 
 export default router;
