24937c6f002327947d4c3a88ca05836ee2ee3966
yassine belkaid <yassine.belkaid87@gmail.com>
2019-06-18 17:19:54 +0100

    [client] converted Menu to functional component

--- client/src/components/Menu/Menu.js
+++ client/src/components/Menu/Menu.js
@@ -1,40 +1,37 @@
-import React, { PureComponent } from 'react';
+import React, { useEffect } from 'react';
 import { NavLink } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import { Spinner } from 'reactstrap';
 
-import { fetchCategories } from '../../store/actions';
+import * as actions from '../../store/actions';
 
-class Menu extends PureComponent {
-    componentDidMount() {
-        this.props.fetchCategories();
-    }
-    render() {
-        const { loading, categories } = this.props;
-        return (
-            <div className="navbar navbar-expand-md navbar-light bg-light justify-content-center">
-                <ul className="nav">
-                    {loading && <Spinner size="sm" color="primary" />}
-                    {categories &&
-                        categories.length > 0 &&
-                        categories.map(category => {
-                            return (
-                                <li className="nav-item" key={category._id}>
-                                    <NavLink
-                                        className="nav-link"
-                                        to={`/category/${category._id}`}>
-                                        {' '}
-                                        {category.name}
-                                    </NavLink>
-                                </li>
-                            );
-                        })}
-                </ul>
-            </div>
-        );
-    }
-}
+const Menu = ({ loading, categories, fetchCategoriesAction }) => {
+    useEffect(() => {
+        fetchCategoriesAction();
+    }, []);
+
+    return (
+        <div className="navbar navbar-expand-md navbar-light bg-light justify-content-center">
+            <ul className="nav">
+                {loading && <Spinner size="sm" color="primary" />}
+                {categories.length > 0 &&
+                    categories.map(category => {
+                        return (
+                            <li className="nav-item" key={category._id}>
+                                <NavLink
+                                    className="nav-link"
+                                    to={`/category/${category._id}`}>
+                                    {' '}
+                                    {category.name}
+                                </NavLink>
+                            </li>
+                        );
+                    })}
+            </ul>
+        </div>
+    );
+};
 
 const mapStateToProps = state => {
     return {
@@ -43,8 +40,10 @@ const mapStateToProps = state => {
     };
 };
 
-const mapDispatchToProps = dispatch =>
-    bindActionCreators({ fetchCategories }, dispatch);
+const mapDispatchToProps = dispatch => {
+    const fetchCategoriesAction = actions.fetchCategories;
+    return bindActionCreators({ fetchCategoriesAction }, dispatch);
+};
 
 export default connect(
     mapStateToProps,
