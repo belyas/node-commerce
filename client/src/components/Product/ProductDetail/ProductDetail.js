bc08d1c8331ae27ba06cbeb85fc966347481006b
yassine belkaid <yassine.belkaid87@gmail.com>
2019-06-19 10:59:02 +0100

    [client] designed product details page

--- client/src/components/Product/ProductDetail/ProductDetail.js
+++ client/src/components/Product/ProductDetail/ProductDetail.js
@@ -1,15 +1,31 @@
 import React from 'react';
-import { Spinner } from 'reactstrap';
+import { Spinner, Button } from 'reactstrap';
 
 const ProductDetail = ({ product, product_id, loading }) => {
     return (
         <div>
             {loading && <Spinner color="primary" />}
-            {!loading && (
-                <>
-                    Product detail of component: {product_id}{' '}
-                    {product && product.name}{' '}
-                </>
+            {!loading && product && (
+                <div id="product-details" className="col-md-12">
+                    <div className="col-md-6 col-sm-12">
+                        <img
+                            src={product.image}
+                            className="img-responsive"
+                            alt={product.name}
+                        />
+                    </div>
+                    <div className="col-md-6 col-sm-12">
+                        <h4>{product.name}</h4>
+                        {product.quantity && (
+                            <p className="product-available">Available</p>
+                        )}
+                        <p className="product-desc">{product.description}</p>
+                        <p className="product-price">${product.price}</p>
+                        <div id="add-to-cart">
+                            <Button color="info"> Add to cart</Button>
+                        </div>
+                    </div>
+                </div>
             )}
         </div>
     );
