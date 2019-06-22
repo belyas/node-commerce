8303ae936cea2f0823d73252bc75a84d1eadc7a2
yassine belkaid <yassine.belkaid87@gmail.com>
2019-06-22 17:37:50 +0100

    [client] fixed potential bug for redux devtools if not installed

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -7,7 +7,7 @@ import rootSaga from '../sagas';
 
 const composeEnhancers =
     process.env.NODE_ENV === 'development'
-        ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+        ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
         : null || compose;
 
 const sagaMiddleware = createSagaMiddleware();
