4402ac81223f492e603c5e7a081b1d78c7a8c008
yassine belkaid <yassine.belkaid87@gmail.com>
2019-06-22 19:00:02 +0100

    [client] typechecking ProductDetail container

--- client/src/containers/Products/ProductDetail/ProductDetait.js
+++ client/src/containers/Products/ProductDetail/ProductDetait.js
@@ -1,22 +1,35 @@
 import React, { useEffect } from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
+import PropTypes from 'prop-types';
 
 import ProductDetailComponent from '../../../components/Product/ProductDetail/ProductDetail';
 import { getProduct } from '../../../store/actions';
 
-const ProductDetail = props => {
-    const { match, getProduct, product } = props;
+const ProductDetail = ({ match, getProduct, product, loading }) => {
     const product_id = match.params.product_id;
+    const _props = { loading, product };
+    const isProductEmpty = Object.keys(product).length === 0;
 
     useEffect(() => {
-        if (!product) {
+        if (isProductEmpty) {
             getProduct(product_id);
         }
         // eslint-disable-next-line
-    }, []);
+    }, [product_id]);
 
-    return <ProductDetailComponent {...props} product_id={product_id} />;
+    return !isProductEmpty && <ProductDetailComponent {..._props} />;
+};
+
+ProductDetail.defaultProps = {
+    product: {},
+};
+
+ProductDetail.propTypes = {
+    loading: PropTypes.bool.isRequired,
+    product: PropTypes.object.isRequired,
+    getProduct: PropTypes.func.isRequired,
+    match: PropTypes.object,
 };
 
 const mapStateToProps = (state, { match }) => {
