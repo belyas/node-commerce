4911182542a1875995944f4b0c7324cc03c29765
yassine belkaid <yassine.belkaid87@gmail.com>
2019-06-22 01:21:05 +0100

    [client] setup saga middleware to listen for actions

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -1,16 +1,22 @@
 import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
+import createSagaMiddleware from 'redux-saga';
 
 import rootReducer from './reducers';
+import rootSaga from '../sagas';
 
 const composeEnhancers =
     process.env.NODE_ENV === 'development'
         ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
         : null || compose;
 
+const sagaMiddleware = createSagaMiddleware();
+
 const store = createStore(
     rootReducer,
-    composeEnhancers(applyMiddleware(thunk))
+    composeEnhancers(applyMiddleware(thunk, sagaMiddleware))
 );
+
+sagaMiddleware.run(rootSaga);
 
 export default store;
