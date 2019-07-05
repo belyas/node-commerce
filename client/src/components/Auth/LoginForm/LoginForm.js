fe39801d4ad7d2ade1a30fb935e81c958fe55e20
yassine belkaid <yassine.belkaid87@gmail.com>
2019-07-05 08:42:52 +0100

    [client] created Login form component for login component

---
+++ client/src/components/Auth/LoginForm/LoginForm.js
@@ -0,0 +1,63 @@
+import React from 'react';
+
+import { concatClasses } from '../../../utils/helpers';
+import styles from '../Login/Login.module.css';
+
+const LoginForm = ({
+    isLogin,
+    errors,
+    email,
+    password,
+    error,
+    blurHandler,
+    changeHandler,
+    submitHanlder,
+    hideForm,
+}) => {
+    return (
+        <form onSubmit={submitHanlder} style={isLogin ? {} : hideForm}>
+            <h2
+                className={concatClasses(
+                    styles.fadeIn,
+                    styles.first,
+                    styles.formTitle
+                )}>
+                Login
+            </h2>
+            <input
+                type="text"
+                className={concatClasses(styles.fadeIn, styles.second)}
+                name="email"
+                placeholder="Email address"
+                value={email}
+                onChange={changeHandler}
+                onBlur={blurHandler}
+                required
+            />
+            {errors && errors.email && (
+                <p className="alert alert-danger">{errors.email}</p>
+            )}
+            <input
+                type="password"
+                className={concatClasses(styles.fadeIn, styles.third)}
+                name="password"
+                placeholder="Password"
+                value={password}
+                onChange={changeHandler}
+                onBlur={blurHandler}
+                required
+            />
+            {errors && errors.password && (
+                <p className="alert alert-danger">{errors.password}</p>
+            )}
+            <input
+                type="submit"
+                className={concatClasses(styles.fadeIn, styles.fourth)}
+                value="Log In"
+            />
+            {error && <p className="alert alert-danger">{error}</p>}
+        </form>
+    );
+};
+
+export default LoginForm;
