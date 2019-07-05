cc908957c477a819fbe3bbb93ac440a66c0a4f73
yassine belkaid <yassine.belkaid87@gmail.com>
2019-07-05 08:46:31 +0100

    [client] fixed unit test for Login component

--- client/src/__tests__/components/Auth/Login/Login.js
+++ client/src/__tests__/components/Auth/Login/Login.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { shallow, mount, render } from 'enzyme';
 
 import Login from '../../../../components/Auth/Login/Login';
-import { wrap } from 'module';
 
 describe('Login component', () => {
     it('should display three inputs and h2 login heading', () => {
@@ -19,13 +18,11 @@ describe('Login component', () => {
             toggleLoginHandler: () => ({}),
         };
         const wrapper = shallow(<Login {...props} />);
-        const forms = wrapper.find('form');
-        const loginForm = forms.at(0);
+        const loginForm = wrapper.find('LoginForm');
 
         expect(wrapper.props().isAuthenticated).toBeFalsy();
-        // we have two forms Login/Signup to toggle between
-        expect(forms).toHaveLength(2);
-        expect(loginForm.find('h2').text()).toEqual('Login');
-        expect(loginForm.find('input')).toHaveLength(3);
+        expect(loginForm).toHaveLength(1);
+        // signup form must be hidden
+        expect(wrapper.find('form').prop('style')).toEqual({ display: 'none' });
     });
 });
