9d3dd63338aab6c1b6622d7aae04729c6e1396ba
yassine belkaid <yassine.belkaid87@gmail.com>
2019-07-09 14:30:19 +0100

    [client] import fontawesome globally

--- client/src/index.js
+++ client/src/index.js
@@ -2,9 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'font-awesome/css/font-awesome.min.css';
 
 import store from './store/index';
-import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
