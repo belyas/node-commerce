996980203fa9f4781c3d663419d6b418566d19f8
yassine belkaid <yassine.belkaid87@gmail.com>
2019-07-09 13:28:01 +0100

    [client] added cart reducer

---
+++ client/src/store/reducers/cart.js
@@ -0,0 +1,35 @@
+import {
+    CART_ADD_ITEM,
+    CART_EMPTY_ITEMS,
+    CART_REMOVE_ITEM,
+} from '../actions/actionTypes';
+import {
+    updateObject,
+    updateCart,
+    getCartTotalPrice,
+} from '../../utils/helpers';
+
+const initialState = {
+    items: [],
+    totalPrice: 0,
+};
+
+const addToCart = (state, action) => {
+    const { product } = action.payload;
+    const items = updateCart(state.items, product);
+    return updateObject(state, {
+        items,
+        totalPrice: getCartTotalPrice(items),
+    });
+};
+
+const reducer = (state = initialState, action) => {
+    switch (action.type) {
+        case CART_ADD_ITEM:
+            return addToCart(state, action);
+        default:
+            return state;
+    }
+};
+
+export default reducer;
