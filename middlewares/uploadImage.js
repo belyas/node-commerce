1c4e28ef29acd58caa766943ab3bb918062d095a
yassine belkaid <yassine.belkaid87@gmail.com>
2019-07-10 18:44:28 +0100

    create missing folders

--- middlewares/uploadImage.js
+++ middlewares/uploadImage.js
@@ -1,14 +1,27 @@
 import path from 'path';
 import multer from 'multer';
+import fs from 'fs';
 
-const storage = (distName) => {
+const storage = distName => {
     return multer.diskStorage({
         destination: (req, file, cb) => {
+            if (!fs.existsSync(path.join(__dirname, `../public/images`))) {
+                fs.mkdir(path.join(__dirname, `../public/images`));
+            }
+
+            if (
+                !fs.existsSync(
+                    path.join(__dirname, `../public/images/${distName}`)
+                )
+            ) {
+                fs.mkdir(path.join(__dirname, `../public/images/${distName}`));
+            }
+
             cb(null, path.join(__dirname, `../public/images/${distName}`));
         },
         filename: (req, file, cb) => {
             cb(null, Date.now() + '-' + file.originalname);
-        }
+        },
     });
 };
 
