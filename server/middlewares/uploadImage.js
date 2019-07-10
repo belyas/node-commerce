b35d42bba6e28e3417df520c09fe4876ba23e9e2
yassine belkaid <yassine.belkaid87@gmail.com>
2019-07-10 18:56:20 +0100

    [server] dynamically create missing images folders

--- server/middlewares/uploadImage.js
+++ server/middlewares/uploadImage.js
@@ -1,14 +1,28 @@
 import path from 'path';
 import multer from 'multer';
+import fs from 'fs';
 
-const storage = (distName) => {
+const storage = distName => {
     return multer.diskStorage({
         destination: (req, file, cb) => {
+            if (
+                !fs.existsSync(
+                    path.join(__dirname, `../public/images/${distName}`)
+                )
+            ) {
+                fs.mkdirSync(
+                    path.join(__dirname, `../public/images/${distName}`),
+                    {
+                        recursive: true,
+                    }
+                );
+            }
+
             cb(null, path.join(__dirname, `../public/images/${distName}`));
         },
         filename: (req, file, cb) => {
             cb(null, Date.now() + '-' + file.originalname);
-        }
+        },
     });
 };
 
