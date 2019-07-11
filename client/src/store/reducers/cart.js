14adf7a49233085da8191b322d57e13c29464aa2
yassine belkaid <yassine.belkaid87@gmail.com>
2019-07-11 19:03:18 +0100

    [client] added remove from cart reducer

--- client/src/store/reducers/cart.js
+++ client/src/store/reducers/cart.js
@@ -1,13 +1,10 @@
-import {
-    CART_ADD_ITEM,
-    CART_EMPTY_ITEMS,
-    CART_REMOVE_ITEM,
-} from '../actions/actionTypes';
+import { CART_ADD_ITEM, CART_REMOVE_ITEM } from '../actions/actionTypes';
 import {
     updateObject,
     updateCart,
     getCartTotalPrice,
     getCartTotalItems,
+    removeItemFromCart,
 } from '../../utils/helpers';
 
 const initialState = {
@@ -19,6 +16,18 @@ const initialState = {
 const addToCart = (state, action) => {
     const { product } = action.payload;
     const items = updateCart(state.items, product);
+
+    return updateObject(state, {
+        items,
+        totalPrice: getCartTotalPrice(items),
+        totalItems: getCartTotalItems(items),
+    });
+};
+
+const removeFromCart = (state, action) => {
+    const { product } = action.payload;
+    const items = removeItemFromCart(state.items, product._id);
+
     return updateObject(state, {
         items,
         totalPrice: getCartTotalPrice(items),
@@ -30,6 +39,8 @@ const reducer = (state = initialState, action) => {
     switch (action.type) {
         case CART_ADD_ITEM:
             return addToCart(state, action);
+        case CART_REMOVE_ITEM:
+            return removeFromCart(state, action);
         default:
             return state;
     }
