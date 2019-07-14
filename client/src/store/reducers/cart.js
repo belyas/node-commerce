c97e232272d247f0b4fcaf6912289bd83508b080
yassine belkaid <yassine.belkaid87@gmail.com>
2019-07-15 00:43:19 +0100

    [client] removed localstorage persisting from cart reducer

--- client/src/store/reducers/cart.js
+++ client/src/store/reducers/cart.js
@@ -7,42 +7,32 @@ import {
     removeItemFromCart,
 } from '../../utils/helpers';
 
-const cartData = localStorage.cart ? JSON.parse(localStorage.cart) : null;
-
 const initialState = {
-    items: cartData ? cartData.items : [],
-    totalPrice: cartData ? cartData.totalPrice : 0,
-    totalItems: cartData ? cartData.totalItems : 0,
+    items: [],
+    totalPrice: 0,
+    totalItems: 0,
 };
 
 const addToCart = (state, action) => {
     const { product } = action.payload;
     const items = updateCart(state.items, product);
 
-    const newState = updateObject(state, {
+    return updateObject(state, {
         items,
         totalPrice: getCartTotalPrice(items),
         totalItems: getCartTotalItems(items),
     });
-
-    localStorage.setItem('cart', JSON.stringify(newState));
-
-    return newState;
 };
 
 const removeFromCart = (state, action) => {
     const { product } = action.payload;
     const items = removeItemFromCart(state.items, product._id);
 
-    const newState = updateObject(state, {
+    return updateObject(state, {
         items,
         totalPrice: getCartTotalPrice(items),
         totalItems: getCartTotalItems(items),
     });
-
-    localStorage.setItem('cart', JSON.stringify(newState));
-
-    return newState;
 };
 
 const reducer = (state = initialState, action) => {
